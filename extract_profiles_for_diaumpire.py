cca9c58eb62e38c3a5c7835e90d4b2fad2549b8c
mohsen mollayi <mmollayi@gmail.com>
2019-07-26 22:21:07 +0430

    added changes to extract_profiles to comply with Alex's commit at 2019-07-26

--- extract_profiles_for_diaumpire.py
+++ extract_profiles_for_diaumpire.py
@@ -1,8 +1,7 @@
 import os
 import sys
+
 execfile('read_rowmajor_matrix.py')
-execfile('sg_filter.py')
-execfile('smooth.py')
 
 bfile = sys.argv[1]
 afile = sys.argv[2]
@@ -17,30 +16,28 @@ for line in fin:
 	scannums.append( line.rstrip().split()[-1] )
 fin.close()
 
-print bfile
-#sys.exit(1)
 
-def get_time_bounds( b, t ):
-	start = t-1
-	end = t+1
 
-	T = b.shape[1]
-	while start >= 1:
-		if b[0,start-1] == 0:
-			break
-		if b[0,start-1] > b[0,start]:
-			break
-		start -= 1
-	
-	while end < T:
-		if b[0,end+1] == 0:
-			break
-		if b[0,end+1] > b[0,end]:
-			break
-		end += 1
-	# Start is inclusive, end is inclusive
-	# returns a tuple where it is (inclusive,exclusive)
-	return (start,end+1) 
+def get_time_bounds(b, t):
+    T = b.shape[1]
+    start = t - 1
+    end = min(T, t + 1)
+    while start >= 1:
+        if b[0, start - 1] == 0:
+            break
+        if b[0, start - 1] > b[0, start]:
+            break
+        start -= 1
+
+    while end < (T - 1):
+        if b[0, end + 1] == 0:
+            break
+        if b[0, end + 1] > b[0, end]:
+            break
+        end += 1
+    # Start is inclusive, end is inclusive
+    # returns a tuple where it is (inclusive,exclusive)
+    return (start, end + 1)
 
 if True:
 	print 'Reading ' + bfile
@@ -48,14 +45,15 @@ if True:
 	b = sp.csr_matrix(b)
 	anns = read_tab_delimited(afile)
 
-# ID | precursor number | monoiostopic m/z | charge | alpha | elution profile
+# ID | precursor number | monoiostopic m/z | charge | peak intensity | elution profile
 
 outfile = ms1file.replace('.ms1','_precursorprofiles.txt')
 
 fout = open(outfile,'w')
-fout.write('ID\tprecursor number\tmonoisotopic m/z\tcharge\talpha\telution profile\n')
+fout.write('ID\tprecursor number\tmonoisotopic m/z\tcharge\tpeak alpha\telution profile\n')
 i = 0
 P = len(anns['charge'])
+T = b.shape[0]
 for p in xrange(P):
 	if p % 5000 == 0:
 		print p
@@ -79,16 +77,16 @@ for p in xrange(P):
 		peak_times.append(pt)
 		peak_alphas.append(alpha)
 
-	subb = b[p,:].todense()
+	subb = b[:, p].todense().transpose()
 	for (pt,a) in zip(peak_times,peak_alphas):
 		(start,end) = get_time_bounds( subb, pt ) 
 		if end-start < 3:
 			continue
-		fout.write(str(i)+'\t'+str(p)+'\t'+str(mz)+'\t'+str(z)+'\t'+str(a)+'\t')
+		fout.write(str(i) + '\t' + str(p) + '\t' + str(mz) + '\t' + str(z) + '\t' + str(a) + '\t')
 		i += 1
-		fout.write( str(scannums[start])+':'+str(subb[0,start]) )
-		for t in range(start+1,end):
-			fout.write(','+str(scannums[t])+':'+str(subb[0,t]) )	
+		fout.write(str(scannums[start]) + ':' + str(subb[0, start]))
+		for t in range(start + 1, min(T, end)):
+			fout.write(',' + str(scannums[t]) + ':' + str(subb[0, t]))
 		fout.write('\n')
 fout.close()
 
