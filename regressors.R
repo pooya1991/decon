d9da4d13766ef66e530b5373d43db03bdc5b81b8
mohsen mollayi <mmollayi@gmail.com>
2019-08-24 16:43:55 +0430

    fixed a bug regarding regression on single row matrices

--- regressors.R
+++ regressors.R
@@ -19,6 +19,7 @@ regressor <- function(X, Y, clustering_info, drop_zero_rows = TRUE) {
 			feature_coef <- c(feature_coef, list(list(feature = idx_col, coef = coefs_sub)))
 			next()
 		}
+
 		idx_col_sub <- feature_selector(Xc, Yc)
 		if (length(idx_col_sub) == 0) next()
 		Xc <- Xc[, idx_col_sub, drop = FALSE]
@@ -31,37 +32,45 @@ regressor <- function(X, Y, clustering_info, drop_zero_rows = TRUE) {
 
 		coefs_sub <- regressor_per_clust(Xc, Yc)
 		coefs[idx_col[idx_col_sub]] <- coefs_sub
-		feature_coef <- c(feature_coef, list(list(feature = idx_col, coef = coefs_sub)))
 	}
 
 	not_valid_clusters <- distinct(clustering_info[c("feature", "cluster")]) %>%
 		count(cluster) %>%
 		filter(n == 1) %>% pull(cluster) %>% as.integer()
 
+	coefs2 <- coefs
 	for (clust in not_valid_clusters) {
 		idx_col <- filter(clustering_info, cluster == clust) %>% pull(feature) %>% unique()
 		idx_row <- rowSums(X[, idx_col, drop = FALSE]) > 0
 		Xc <- X[idx_row, idx_col, drop = FALSE]
 		Yc <- Y[idx_row, , drop = FALSE]
-		mdl <- glmnet::glmnet(Xc, Yc, intercept = FALSE, lower.limits = 0, lambda = 0)
-		coefs_sub <- predict(mdl, type = "coef") %>% as.vector() %>% .[-1]
-		feature_coef <- c(feature_coef, list(list(feature = idx_col, coef = coefs_sub)))
+		if (nrow(Xc) == 1) {
+			# browser()
+			coefs_sub <- Yc[, 1] / Xc[, 1]
+		} else {
+			mdl <- glmnet::glmnet(Xc, Yc, intercept = FALSE, lower.limits = 0, lambda = 0)
+			coefs_sub <- predict(mdl, type = "coef") %>% as.vector() %>% .[-1]
+		}
+		coefs2[idx_col] <- coefs_sub
 	}
 
-	coefs2 <<- map(feature_coef, as_tibble, .name_repair = "unique") %>%
-		bind_rows() %>% arrange(feature) %>% pull(2)
+	coefs2 <<- coefs2
 	coefs
 }
 
 
 feature_selector <- function(X, Y) {
-	mdl <- glmnet::cv.glmnet(X, Y, intercept = TRUE, lower.limits = 0,
-							 nfolds = nrow(X), grouped = FALSE, type.measure = "mae",
-							 weights = 1 / (10 + drop(Y)))
+	mdl <- tryCatch({
+		glmnet::cv.glmnet(X, Y, intercept = TRUE, lower.limits = 0,
+						  nfolds = nrow(X), grouped = FALSE, type.measure = "mae",
+						  weights = 1 / (10 + drop(Y))
+		)},
+		error = function(e) NULL)
 
-	idx_col<- predict(mdl, type = "coef", s = "lambda.min") %>%
+	if (is.null(mdl)) return(1:ncol(X))
+
+	idx_col <- predict(mdl, type = "coef", s = "lambda.min") %>%
 		as.vector() %>% (function(x) which(x[-1] > 0))
-	idx_col <- 1:ncol(X)
 	idx_col
 }
 
